e23e8c076f149369a824ecf8807d034fe5e9cffa
Ricardo Guerrero <richi_gro@hotmail.com>
2018-09-23 18:55:49 -0500

    added new route /recipes for GET requests

--- server.js
+++ server.js
@@ -31,6 +31,12 @@ app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
 
+app.get("/recipes", (req, res) => {
+  res.json(recipes.get());
+});
+
+
+
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
